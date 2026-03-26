8667f00128f5a0d30e3ff33278a0e68d909dee46
James Rossini <james.rossini@posteo.de>
2026-03-26 09:21:48 +0100

    GetStockAccount:  - Used helper module (cf. commit c1b410c6968c93158d300ff354cf56dd58c82683,    forgot to check in there).  - Adapted to changes in commit 80c3202a14c5d08dd6240e300aed5abe825c7eb0
    Cf. sister module, commit 36ad5804b2f0640586422ead96d3d8e1281af593.

--- src/main/java/org/kmymoney/tools/xml/get/sonstige/GetStockAcct.java
+++ src/main/java/org/kmymoney/tools/xml/get/sonstige/GetStockAcct.java
@@ -2,7 +2,6 @@ package org.kmymoney.tools.xml.get.sonstige;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -15,14 +14,17 @@ import org.apache.commons.configuration.PropertiesConfiguration;
 import org.kmymoney.api.read.KMyMoneyAccount;
 import org.kmymoney.api.read.KMyMoneySecurity;
 import org.kmymoney.api.read.impl.KMyMoneyFileImpl;
-import org.kmymoney.base.basetypes.complex.KMMComplAcctID;
+import org.kmymoney.apiext.secacct.SecuritiesAccountManager;
+import org.kmymoney.base.basetypes.simple.KMMAcctID;
 import org.kmymoney.base.basetypes.simple.KMMSecID;
 import org.kmymoney.tools.CommandLineTool;
+import org.kmymoney.tools.xml.helper.AccountHelper;
+import org.kmymoney.tools.xml.helper.CmdLineHelper_Acct;
+import org.kmymoney.tools.xml.helper.CmdLineHelper_Sec;
+import org.kmymoney.tools.xml.helper.SecurityHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import xyz.schnorxoborx.base.beanbase.NoEntryFoundException;
-import xyz.schnorxoborx.base.beanbase.TooManyEntriesFoundException;
 import xyz.schnorxoborx.base.cmdlinetools.CouldNotExecuteException;
 import xyz.schnorxoborx.base.cmdlinetools.Helper;
 import xyz.schnorxoborx.base.cmdlinetools.InvalidCommandLineArgsException;
@@ -39,14 +41,30 @@ public class GetStockAcct extends CommandLineTool
   
   private static String                kmmFileName  = null;
   
-  private static Helper.Mode           acctMode     = null;
-  private static KMMComplAcctID        acctID       = null;
-  private static String                acctName     = null;
+  // ---
   
-  private static Helper.CmdtySecSingleSelMode secMode = null;
-  private static KMMSecID              secID        = null;
-  private static String                isin         = null;
-  private static String                secName      = null;
+  private static Helper.Mode           acctSelMode  = null;
+
+  private static KMMAcctID             acctID       = new KMMAcctID(); // sic, not KMMComplAcctID
+  // This one and the following: sic, StringBuffer, not String,
+  // for it has to be mutable because of the way the args are parsed.
+  private static StringBuffer          acctName     = new StringBuffer();
+  
+  // ---
+  
+  private static Helper.CmdtySecSingleSelMode secSelMode = null;
+
+  private static KMMSecID              secID        = new KMMSecID();
+  // This one and the following: sic, StringBuffer, not String,
+  // for it has to be mutable because of the way the args are parsed.
+  private static StringBuffer          isin         = new StringBuffer();
+  // Possibly later:
+  // private static String  wkn      = new StringBuffer();
+  // private static String  cusip    = new StringBuffer();
+  // private static String  sedol    = new StringBuffer();
+  private static StringBuffer          secName      = new StringBuffer();
+  
+  // ---
   
   private static boolean scriptMode = false;
 
@@ -82,23 +100,17 @@ public class GetStockAcct extends CommandLineTool
       .desc("KMyMoney file")
       .longOpt("kmymoney-file")
       .get();
+    
+    // ---
       
-    Option optAcctMode = Option.builder("am")
+    Option optAcctMode = Option.builder("asm")
       .required()
       .hasArg()
       .argName("mode")
       .desc("Selection mode for account")
-      .longOpt("account-mode")
+      .longOpt("acct-sel-mode")
       .get();
       
-    Option optSecMode = Option.builder("sm")
-      .required()
-      .hasArg()
-      .argName("mode")
-      .desc("Selection mode for security")
-      .longOpt("security-mode")
-      .get();
-        
     Option optAcctID = Option.builder("acct")
       .hasArg()
       .argName("acctid")
@@ -112,28 +124,42 @@ public class GetStockAcct extends CommandLineTool
       .desc("Account name (or part of)")
       .longOpt("account-name")
       .get();
-      
+
+    // ---
+    
+    Option optSecMode = Option.builder("ssm")
+      .required()
+      .hasArg()
+      .argName("mode")
+      .desc("Selection mode for security")
+      .longOpt("sec-sel-mode")
+      .get();
+
     Option optSecID = Option.builder("sec")
       .hasArg()
-      .argName("ID")
-      .desc("Security ID")
+      .argName("secid")
+      .desc("Security ID (direct)" +
+      		"(for <mode> = " + Helper.CmdtySecSingleSelMode.ID + " only)")
       .longOpt("security-id")
       .get();
-            
+
     Option optSecISIN = Option.builder("is")
       .hasArg()
       .argName("isin")
-      .desc("ISIN")
+      .desc("ISIN " + 
+      		"(Security ID indirect). " +
+  		   	"(for <mode> = " + Helper.CmdtySecSingleSelMode.ISIN + " only)")
       .longOpt("isin")
       .get();
-          
+
     Option optSecName = Option.builder("sn")
       .hasArg()
       .argName("name")
-      .desc("Security name (or part of)")
+      .desc("Security name (full) " + 
+  		    "(for <mode> = " + Helper.CmdtySecSingleSelMode.NAME + " only)")
       .longOpt("security-name")
       .get();
-            
+
     // The convenient ones
     Option optScript = Option.builder("sl")
       .desc("Script Mode")
@@ -161,92 +187,35 @@ public class GetStockAcct extends CommandLineTool
   @Override
   protected void kernel() throws Exception
   {
-    KMyMoneyFileImpl kmmFile = new KMyMoneyFileImpl(new File(kmmFileName), true);
+    KMyMoneyFileImpl kmmFile = new KMyMoneyFileImpl(new File(kmmFileName), ! scriptMode);
 
-    KMyMoneyAccount acct = null;
-    
-    if (acctMode == Helper.Mode.ID)
-    {
-      acct = kmmFile.getAccountByID(acctID);
-      if (acct == null)
-      {
-        if ( ! scriptMode )
-          System.err.println("Found no account with that name");
-        throw new NoEntryFoundException();
-      }
-    }
-    else if (acctMode == Helper.Mode.NAME)
-    {
-      Collection<KMyMoneyAccount> acctList = null;
-      acctList = kmmFile.getAccountsByTypeAndName(KMyMoneyAccount.Type.INVESTMENT, acctName, 
-                                                  true, true);
-      if (acctList.size() == 0)
-      {
-        if ( ! scriptMode )
-        {
-          System.err.println("Could not find accounts matching this name.");
-        }
-        throw new NoEntryFoundException();
-      }
-      else if (acctList.size() > 1)
-      {
-        if ( ! scriptMode )
-        {
-          System.err.println("Found " + acctList.size() + " accounts with that name.");
-          System.err.println("Please specify more precisely.");
-        }
-        throw new TooManyEntriesFoundException();
-      }
-      acct = acctList.iterator().next();
-    }
+    // ---
+
+    KMyMoneyAccount acct = AccountHelper.getAcct(acctSelMode,
+												acctID, acctName.toString(), 
+												kmmFile,
+												scriptMode);
 
     if ( ! scriptMode )
       System.out.println("Account:  " + acct.toString());
     
-    // ----------------------------
+    // ---
 
-    KMyMoneySecurity sec = null;
-    
-    if ( secMode == Helper.CmdtySecSingleSelMode.ID )
-    {
-      sec = kmmFile.getSecurityByID(secID);
-      if ( sec == null )
-      {
-        if ( ! scriptMode )
-          System.err.println("Could not find a security with this ID.");
-        throw new NoEntryFoundException();
-      }
-    }
-    else if ( secMode == Helper.CmdtySecSingleSelMode.ISIN )
-    {
-      sec = kmmFile.getSecurityByCode(isin);
-      if ( sec == null )
-      {
-        if ( ! scriptMode )
-          System.err.println("Could not find securities with this ISIN.");
-        throw new NoEntryFoundException();
-      }
-    }
-    else if ( secMode == Helper.CmdtySecSingleSelMode.NAME )
-    {
-      sec = kmmFile.getSecurityByNameUniq(secName);
-      if ( sec == null )
-      {
-        if ( ! scriptMode )
-          System.err.println("Could not find security (uniquely) matching this name.");
-        throw new NoEntryFoundException();
-      }
-    }
-    
+    KMyMoneySecurity sec = SecurityHelper.getSec(secSelMode,
+												secID, isin.toString(), secName.toString(), 
+												kmmFile,
+												scriptMode);
+
     if ( ! scriptMode )
       System.out.println("Security: " + sec.toString());
     
     // ----------------------------
     
-    for ( KMyMoneyAccount subAcct : acct.getChildren() ) {
-      if ( subAcct.getType() == KMyMoneyAccount.Type.STOCK &&
-           subAcct.getQualifSecCurrID().equals(sec.getQualifID()) ) {
-          System.out.println(subAcct.getID());
+    SecuritiesAccountManager secAcctMgr = new SecuritiesAccountManager(acct);
+    
+    for ( KMyMoneyAccount chld : secAcctMgr.getShareAccts(true) ) { // ::TODO: optionally non-active accounts 
+      if ( chld.getQualifSecCurrID().equals( sec.getQualifID() ) ) {
+          System.out.println(chld.getID());
       }
     }
 
@@ -294,188 +263,59 @@ public class GetStockAcct extends CommandLineTool
     if ( ! scriptMode )
       System.err.println("KMyMoney file:     '" + kmmFileName + "'");
     
-    // <account-mode>
+    // ----------------------------
+
+    // <acct-sel-mode>
     try
     {
-      acctMode = Helper.Mode.valueOf(cmdLine.getOptionValue("account-mode"));
+      acctSelMode = Helper.Mode.valueOf(cmdLine.getOptionValue("acct-sel-mode"));
     }
     catch ( Exception exc )
     {
-      System.err.println("Could not parse <account-mode>");
+      System.err.println("Could not parse <acct-sel-mode>");
       throw new InvalidCommandLineArgsException();
     }
     
     if ( ! scriptMode )
-      System.err.println("Account mode:  " + acctMode);
-
-    // <security-mode>
+      System.err.println("Account mode:      " + acctSelMode);
+    
+  	// ---------
+  	
+    // <sec-sel-mode>
     try
     {
-      secMode = Helper.CmdtySecSingleSelMode.valueOf(cmdLine.getOptionValue("security-mode"));
+      secSelMode = Helper.CmdtySecSingleSelMode.valueOf(cmdLine.getOptionValue("sec-sel-mode"));
     }
     catch ( Exception exc )
     {
-      System.err.println("Could not parse <security-mode>");
+      System.err.println("Could not parse <sec-sel-mode>");
       throw new InvalidCommandLineArgsException();
     }
     
     if ( ! scriptMode )
-      System.err.println("Security mode: " + secMode);
+      System.err.println("Security mode:     " + secSelMode);
 
-    // <account-id>
-    if ( cmdLine.hasOption("account-id") )
-    {
-      if ( acctMode != Helper.Mode.ID )
-      {
-        System.err.println("<account-id> must only be set with <account-mode> = '" + Helper.Mode.ID.toString() + "'");
-        throw new InvalidCommandLineArgsException();
-      }
-      
-      try
-      {
-        acctID = new KMMComplAcctID( cmdLine.getOptionValue("account-id") );
-      }
-      catch ( Exception exc )
-      {
-        System.err.println("Could not parse <account-id>");
-        throw new InvalidCommandLineArgsException();
-      }
-    }
-    else
-    {
-      if ( acctMode == Helper.Mode.ID )
-      {
-        System.err.println("<account-id> must be set with <account-mode> = '" + Helper.Mode.ID.toString() + "'");
-        throw new InvalidCommandLineArgsException();
-      }      
-    }
-    
-    if ( ! scriptMode )
-      System.err.println("Account ID:    '" + acctID + "'");
+  	// ---------
 
-    // <account-name>
-    if ( cmdLine.hasOption("account-name") )
-    {
-      if ( acctMode != Helper.Mode.NAME )
-      {
-        System.err.println("<account-name> must only be set with <account-mode> = '" + Helper.Mode.NAME.toString() + "'");
-        throw new InvalidCommandLineArgsException();
-      }
-      
-      try
-      {
-        acctName = cmdLine.getOptionValue("account-name");
-      }
-      catch ( Exception exc )
-      {
-        System.err.println("Could not parse <name>");
-        throw new InvalidCommandLineArgsException();
-      }
-    }
-    else
-    {
-      if ( acctMode == Helper.Mode.NAME )
-      {
-        System.err.println("<account-name> must be set with <account-mode> = '" + Helper.Mode.NAME.toString() + "'");
-        throw new InvalidCommandLineArgsException();
-      }      
-    }
-    
-    if ( ! scriptMode )
-      System.err.println("Account name:  '" + acctName + "'");
+    // <acct-sel-mode>
+    // <account-id>, <acct-name>
+    CmdLineHelper_Acct.parseAcctStuffWrap( cmdLine, 
+    								 acctSelMode, 
+    								 acctID, acctName, 
+    								 scriptMode );
 
-    // <security-id>
-    if ( cmdLine.hasOption("security-id") )
-    {
-      if ( secMode != Helper.CmdtySecSingleSelMode.ID )
-      {
-        System.err.println("<security-id> must only be set with <security-mode> = '" + Helper.CmdtySecSingleSelMode.ID.toString() + "'");
-        throw new InvalidCommandLineArgsException();
-      }
-      
-      try
-      {
-        secID = new KMMSecID( cmdLine.getOptionValue("security-id") );
-      }
-      catch (Exception exc)
-      {
-        System.err.println("Could not parse <security-id>");
-        throw new InvalidCommandLineArgsException();
-      }
-    }
-    else
-    {
-      if ( secMode == Helper.CmdtySecSingleSelMode.ID )
-      {
-        System.err.println("<security-id> must be set with <security-mode> = '" + Helper.CmdtySecSingleSelMode.ID.toString() + "'");
-        throw new InvalidCommandLineArgsException();
-      }
-    }
+  	// ---------
 
-    if (!scriptMode)
-      System.err.println("Security ID:   '" + secID + "'");
-
-    // <isin>
-    if ( cmdLine.hasOption("isin") )
-    {
-      if ( secMode != Helper.CmdtySecSingleSelMode.ISIN )
-      {
-        System.err.println("<isin> must only be set with <security-mode> = '" + Helper.CmdtySecSingleSelMode.ISIN.toString() + "'");
-        throw new InvalidCommandLineArgsException();
-      }
-      
-      try
-      {
-        isin = cmdLine.getOptionValue("isin");
-      }
-      catch (Exception exc)
-      {
-        System.err.println("Could not parse <isin>");
-        throw new InvalidCommandLineArgsException();
-      }
-    }
-    else
-    {
-      if ( secMode == Helper.CmdtySecSingleSelMode.ISIN )
-      {
-        System.err.println("<isin> must be set with <security-mode> = '" + Helper.CmdtySecSingleSelMode.ISIN.toString() + "'");
-        throw new InvalidCommandLineArgsException();
-      }
-    }
-
-    if (!scriptMode)
-      System.err.println("Security ISIN: '" + isin + "'");
-
-    // <security-name>
-    if ( cmdLine.hasOption("security-name") )
-    {
-      if ( secMode != Helper.CmdtySecSingleSelMode.NAME )
-      {
-        System.err.println("<security-name> must only be set with <security-mode> = '" + Helper.CmdtySecSingleSelMode.NAME.toString() + "'");
-        throw new InvalidCommandLineArgsException();
-      }
-      
-      try
-      {
-        secName = cmdLine.getOptionValue("security-name");
-      }
-      catch (Exception exc)
-      {
-        System.err.println("Could not parse <security-name>");
-        throw new InvalidCommandLineArgsException();
-      }
-    }
-    else
-    {
-      if ( secMode == Helper.CmdtySecSingleSelMode.NAME )
-      {
-        System.err.println("<security-name> must be set with <security-mode> = '" + Helper.CmdtySecSingleSelMode.NAME.toString() + "'");
-        throw new InvalidCommandLineArgsException();
-      }
-    }
-
-    if (!scriptMode)
-      System.err.println("Security name: '" + secName + "'");
+    // <sec-sel-mode>, <sec-sel-sub-mode>,
+    // <sec-id>,
+    // <isin>,
+    // <sec-name>
+    CmdLineHelper_Sec.parseSecStuffWrap( cmdLine, 
+    								 secSelMode, null,
+    								 secID, 
+    								 isin, 
+    								 secName, 
+    								 scriptMode );
   }
   
   @Override
@@ -493,12 +333,12 @@ public class GetStockAcct extends CommandLineTool
 	}
     
     System.out.println("");
-    System.out.println("Valid values for <account-mode>:");
+    System.out.println("Valid values for <acct-sel-mode>:");
     for ( Helper.Mode elt : Helper.Mode.values() )
       System.out.println(" - " + elt);
 
     System.out.println("");
-    System.out.println("Valid values for <security-mode>:");
+    System.out.println("Valid values for <sec-sel-mode>:");
     for ( Helper.CmdtySecSingleSelMode elt : Helper.CmdtySecSingleSelMode.values() )
       System.out.println(" - " + elt);
   }
